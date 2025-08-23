53848e7ab808fb1ba5bff4c59f43be69a0b7e41e
차태영 <3hjab@naver.com>
2025-08-23 18:54:23 +0900

    feat: :art: 바벨 설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { useState } from "react";
+import Test from "./Test";
 
 export default function App() {
   const [count, setCount] = useState(0);
 
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Test />
+    </div>
+  );
 }
